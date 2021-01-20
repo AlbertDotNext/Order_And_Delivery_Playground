35b8ee9bd0ee802c055d19782550cfb2c76ab11b
Albert <albertwu0001@gmail.com>
2021-01-19 20:27:25 -0800

    fill the tab bar gap in the bottom

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -67,6 +67,27 @@ const TabBarCustomButton = ({accessibilityState, children, onPress}) => {
   }
 }
 
+const CustomTabBar = (props) => {
+  return (
+    <View>
+      <View
+       style={{
+         position: 'absolute',
+         bottom: 0,
+         left: 0,
+         right: 0,
+         height: 30,
+         backgroundColor: COLORS.white
+       }}
+      >
+
+      </View>
+      <BottomTabBar {...props.props} />
+    </View>
+
+  )
+}
+
 const Tabs = () => {
   return (
     <Tab.Navigator
@@ -78,6 +99,11 @@ const Tabs = () => {
           elevation: 0,
         }
       }}
+      tabBar={(props) => (
+        <CustomTabBar
+         props={props}
+        />
+      )}
     >
       <Tab.Screen
       name="Home"
