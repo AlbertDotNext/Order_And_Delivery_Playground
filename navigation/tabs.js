bd613afe4616352d8456517f3de813b3ba71e0dd
Albert <albertwu0001@gmail.com>
2021-01-19 20:20:17 -0800

    modified button shape on tab bar

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -11,13 +11,72 @@ import { Home } from "../screens"
 
 import { COLORS, icons} from "../constants"
 
+import Svg, {Path} from 'react-native-svg'
 const Tab = createBottomTabNavigator();
+
+const TabBarCustomButton = ({accessibilityState, children, onPress}) => {
+  var isSelected = accessibilityState.selected
+  if(isSelected) {
+    return (
+      <View style={{ flex:1, alignItems: "center"}}>
+        <View style={{ flexDirection: 'row', position: 'absolute', top: 0}}>
+          <View style={{flex: 1, backgroundColor: COLORS.white}}></View>
+          <Svg
+            width={75}
+            height={61}
+            viewBox="0 0 75 61"
+          >
+            <Path
+              d="M75.2 0v61H0v0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
+              fill={COLORS.white}
+            />
+          </Svg>
+          <View style={{flex:1, backgroundColor: COLORS.white}}></View>
+
+        </View>
+        <TouchableOpacity
+          style={{
+            top: -22.5,
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: 50,
+            height: 50,
+            borderRadius: 25,
+            backgroundColor: COLORS.white
+          }}
+          onPress={onPress}
+        >
+          {children}
+        </TouchableOpacity>
+      </View>
+    )
+  } else {
+    return (
+      <TouchableOpacity
+        style={{
+          flex: 1,
+          height: 60,
+          backgroundColor: COLORS.white
+        }}
+        activeOpactiy={1}
+        onPress={onPress}
+      >
+        {children}
+      </TouchableOpacity>
+    )
+  }
+}
 
 const Tabs = () => {
   return (
     <Tab.Navigator
       tabBarOptions={{
-        showLabel: false
+        showLabel: false,
+        style: {
+          borderTopWidth: 0,
+          backgroundColor: "transparent",
+          elevation: 0,
+        }
       }}
     >
       <Tab.Screen
@@ -33,6 +92,11 @@ const Tabs = () => {
             height: 25,
             tintColor: focused ? COLORS.primary : COLORS.secondary
           }}
+          />
+        ),
+        tabBarButton: (props) => (
+          <TabBarCustomButton
+            {...props}
           />
         )
       }}
@@ -51,6 +115,11 @@ const Tabs = () => {
             tintColor: focused ? COLORS.primary : COLORS.secondary
           }}
           />
+        ),
+        tabBarButton: (props) => (
+          <TabBarCustomButton
+            {...props}
+          />
         )
       }}
       />
@@ -68,6 +137,11 @@ const Tabs = () => {
             tintColor: focused ? COLORS.primary : COLORS.secondary
           }}
           />
+        ),
+        tabBarButton: (props) => (
+          <TabBarCustomButton
+            {...props}
+          />
         )
       }}
       />
@@ -84,6 +158,11 @@ const Tabs = () => {
             height: 25,
             tintColor: focused ? COLORS.primary : COLORS.secondary
           }}
+          />
+        ),
+        tabBarButton: (props) => (
+          <TabBarCustomButton
+            {...props}
           />
         )
       }}
