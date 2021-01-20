921f4a8265fa86e40f16ac676fb697a41fc1b28a
Albert <albertwu0001@gmail.com>
2021-01-20 15:26:57 -0800

    add function to filter categories by id and styling categories

--- screens/Home.js
+++ screens/Home.js
@@ -345,6 +345,15 @@ const restaurantData = [
     setSelectedCategory(category)
   }
 
+  function getCategoryNameById(id) {
+    let category = categories.filter(a => a.id == id)
+
+    if (category.length > 0)
+            return category[0].name
+
+        return ""
+  }
+
   function renderHeader(){
     return (
       <View style={{ flexDirection: 'row', height: 50}}>
@@ -470,7 +479,11 @@ const restaurantData = [
       <TouchableOpacity
         style={{marginBottom: SIZES.padding * 2}}
       >
-        <View>
+        <View
+          style={{
+            marginBottom: SIZES.padding
+          }}
+        >
           <Image
             source={item.photo}
             resizeMode="cover"
@@ -495,6 +508,55 @@ const restaurantData = [
             }}
           >
             <Text style={{...FONTS.h4}}>{item.duration}</Text>
+          </View>
+        </View>
+        <Text style={{ ...FONTS.body2 }}>{item.name}</Text>
+        <View
+          style={{
+            marginTop: SIZES.padding,
+            flexDirection: 'row'
+          }}
+        >
+          <Image
+            source={icons.star}
+            style={{
+              height: 20,
+              width: 20,
+              tintColor: COLORS.primary,
+              marginRight: 10
+            }}
+          />
+          <Text style={{ ...FONTS.body3}}>{item.rating}</Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              marginLeft: 10
+            }}
+          >
+            {
+              item.categories.map((categoryId) => {
+                return (
+                  <View
+                    style={{ flexDirection: 'row'}}
+                    key={categoryId}
+                  >
+                    <Text style={{ ...FONTS.body3 }}>{getCategoryNameById(categoryId)}</Text>
+                    <Text style={{ ...FONTS.h3, color: COLORS.darkgray}}> . </Text>
+                  </View>
+                )
+              })
+            }
+            {
+              [1, 2, 3].map((priceRating) => (
+                <Text
+                    key={priceRating}
+                    style={{
+                        ...FONTS.body3,
+                        color: (priceRating <= item.priceRating) ? COLORS.black : COLORS.darkgray
+                    }}
+                >$</Text>
+              ))
+            }
           </View>
         </View>
       </TouchableOpacity>
