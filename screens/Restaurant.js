c918130e49431d8bacfc535a8e0b8f3b7f6a322c
Albert <albertwu0001@gmail.com>
2021-01-20 15:59:23 -0800

    add navigation to restaurant page and add header of icon

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -1,18 +1,64 @@
 import React from 'react';
 import {
   View,
-  Text
-
+  Text,
+  Image,
+  TouchableOpacity,
+  SafeAreaView,
+  StyleSheet,
+  Animated
 } from "react-native";
+import {} from 'react-native-iphone-x-helper'
+import {icons, COLORS, SIZES, FONTS} from '../constants'
 
-const Restaurant = () => {
+const Restaurant = ({ route, navigation }) => {
+
+  const [restaurant, setRestaurant ] = React.useState(null);
+  const [currentLocation, setCurrentLocation] = React.useState(null);
+
+  React.useEffect(() => {
+    let { item, currentLocation } = route.params;
+
+    setRestaurant(item)
+    setCurrentLocation(currentLocation)
+  })
+
+  function renderHeader() {
+    return (
+      <View style={{ flexDirection: 'row'}}>
+        <TouchableOpacity
+          style={{
+            width: 50,
+            paddingLeft: SIZES.padding * 2,
+            justifyContent: 'center'
+          }}
+          onPress={() => navigation.goBack()}
+        >
+          <Image
+            source={icons.back}
+            resizeMode="contain"
+            style={{
+              width: 30,
+              height: 30
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
   return (
-    <View>
-      <Text>
-        Restaurant
-      </Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      {renderHeader()}
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      backgroundColor: COLORS.lightGray2
+  }
+})
 
 export default Restaurant;
