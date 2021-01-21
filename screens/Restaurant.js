c148c55cf43864ed9d68b44681327136f7976d55
Albert <albertwu0001@gmail.com>
2021-01-20 16:57:33 -0800

    add item order section and location icon and credit card icon

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -258,6 +258,64 @@ const Restaurant = ({ route, navigation }) => {
     return (
       <View>
         {renderDots()}
+
+        <View
+            style={{
+                backgroundColor: COLORS.white,
+                borderTopLeftRadius: 40,
+                borderTopRightRadius: 40
+            }}
+        >
+            <View
+                style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    paddingVertical: SIZES.padding * 2,
+                    paddingHorizontal: SIZES.padding * 3,
+                    borderBottomColor: COLORS.lightGray2,
+                    borderBottomWidth: 1
+                }}
+            >
+                <Text style={{ ...FONTS.h3 }}>5 items in Cart</Text>
+                <Text style={{ ...FONTS.h3 }}>$45</Text>
+            </View>
+
+            <View
+                style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    paddingVertical: SIZES.padding * 2,
+                    paddingHorizontal: SIZES.padding * 3
+                }}
+            >
+                <View style={{ flexDirection: 'row' }}>
+                    <Image
+                        source={icons.pin}
+                        resizeMode="contain"
+                        style={{
+                            width: 20,
+                            height: 20,
+                            tintColor: COLORS.darkgray
+                        }}
+                    />
+                    <Text style={{ marginLeft: SIZES.padding, ...FONTS.h4 }}>Location</Text>
+                </View>
+
+                <View style={{ flexDirection: 'row' }}>
+                    <Image
+                        source={icons.master_card}
+                        resizeMode="contain"
+                        style={{
+                            width: 20,
+                            height: 20,
+                            tintColor: COLORS.darkgray
+                        }}
+                    />
+                    <Text style={{ marginLeft: SIZES.padding, ...FONTS.h4 }}>8888</Text>
+                </View>
+            </View>
+
+        </View>
       </View>
     )
   }
